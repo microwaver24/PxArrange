e2425645577af93ed62702994689d7f0d4d0febc
microwaver24 <microwaver24@gmail.com>
2024-09-18 17:20:20 -0700

    Start writing tests.
    The code is complex enough that tests would be helpful. I haven't really written any tests yet, but there is a test project and a file for one of the classes.

---
+++ PxArrangeTests/MoveSlideshowImagesTests.cs
@@ -0,0 +1,56 @@
+﻿namespace PxArrange.Tests
+{
+	[TestClass()]
+	public class MoveSlideshowImagesTests
+	{
+		public const string TestDirName = "PxArrangeTests";
+
+		/// <summary>
+		/// This method will be called before each MSTest test method
+		/// </summary>
+		[TestInitialize()]
+		public void Setup()
+		{
+			var tempDir = Path.GetTempPath();
+			var testDirPath = Path.Combine(tempDir, TestDirName);
+			Console.WriteLine($"tempDir [{tempDir}] testDirPath [{testDirPath}]");
+			if (Directory.Exists(testDirPath))
+			{
+				Directory.Delete(testDirPath, true);
+			}
+			Directory.CreateDirectory(testDirPath);
+
+			// create the proper directory structure
+			// make some fake images in the directories
+			// make a fake database?
+			// load a config file for testing with paths pointing at the temp directory
+		}
+
+		/// <summary>
+		/// This method will be called after each MSTest test method has completed
+		/// </summary>
+		[TestCleanup()]
+		public void Teardown()
+		{
+			var tempDir = Path.GetTempPath();
+			var testDirPath = Path.Combine(tempDir, TestDirName);
+			if (Directory.Exists(testDirPath))
+			{
+				Directory.Delete(testDirPath, true);
+			}
+		}
+
+		[TestMethod()]
+		public void MoveSlideshowImagesTest()
+		{
+			var moveSlideshowImages = new MoveSlideshowImages();
+			Assert.IsNotNull(moveSlideshowImages);
+		}
+
+		//[TestMethod()]
+		//public void RunTest()
+		//{
+		//	Assert.Fail();
+		//}
+	}
+}
